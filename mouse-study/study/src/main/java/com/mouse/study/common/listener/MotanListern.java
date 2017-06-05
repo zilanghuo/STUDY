0240ae8220067d3392c8ae483bfbbf0dedd1e6f9
laiwf <laiwf@zendaimoney.com>
2017-06-05 15:53:13 +0800

    添加监听器

---
+++ mouse-study/study/src/main/java/com/mouse/study/common/listener/MotanListern.java
@@ -0,0 +1,29 @@
+package com.mouse.study.common.listener;
+
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by lwf on 2017/6/5.
+ */
+@Slf4j
+public class MotanListern implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        log.info("      *");
+        log.info("    *    *");
+        log.info("  *        *");
+        log.info("*    motan    *");
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+
+    }
+}
