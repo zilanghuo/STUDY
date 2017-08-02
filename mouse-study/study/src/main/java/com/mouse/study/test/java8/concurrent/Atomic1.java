01d4888802611dcb6433fe66924e76ce2597e2c9
laiwf <laiwf@zendaimoney.com>
2017-08-02 17:43:05 +0800

    -->线程例子

--- mouse-study/study/src/main/java/com/mouse/study/test/java8/concurrent/Atomic1.java
+++ mouse-study/study/src/main/java/com/mouse/study/test/java8/concurrent/Atomic1.java
@@ -1,5 +1,7 @@
 package com.mouse.study.test.java8.concurrent;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -18,6 +20,7 @@ public class Atomic1 {
         testIncrement();
         testAccumulate();
         testUpdate();
+        testDemo();
     }
 
     private static void testUpdate() {
@@ -65,6 +68,35 @@ public class Atomic1 {
         ConcurrentUtils.stop(executor);
 
         System.out.format("Increment: Expected=%d; Is=%d\n", NUM_INCREMENTS, atomicInt.get());
+    }
+
+    private static void testDemo() {
+        ExecutorService executor = Executors.newFixedThreadPool(2);
+
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println("new Runnable");
+            }
+        });
+        executor.execute(() -> System.out.println("---> Runnable"));
+
+    }
+
+    private static void testList() {
+        //Old way:
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        int sum = 0;
+        for (Integer n : list) {
+            int x = n * n;
+            sum = sum + x;
+        }
+        System.out.println(sum);
+
+        //New way:
+        List<Integer> list2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+        int sum2 = list2.stream().map(x -> x * x).reduce((x, y) -> x + y).get();
+        System.out.println(sum2);
     }
 
 }
