5937b3cc8d625d30acecd60fbcc0f41146f86ad0
laiwf <laiwf@zendaimoney.com>
2017-08-07 15:54:54 +0800

    删除idea文件

---
+++ mouse-study/study/src/main/java/com/mouse/study/test/mode/templet/Main.java
@@ -0,0 +1,20 @@
+package com.mouse.study.test.mode.templet;
+
+/**
+ * Created by lwf on 2017/8/4.
+ * use to do:
+ */
+public class Main {
+    public static void main(String[] args) {
+        TemplateAbstract stringDisplay = new StringDisplay();
+
+        TemplateAbstract integerDisplay = new IntegerDisplay();
+
+        stringDisplay.display();
+
+        integerDisplay.display();
+
+        System.out.println("尊敬的刘大鹏，您投资订单编号为819756的产品9%稀缺产品962910期将收回本息104,463.01元，其中本金100,000元，利息4,463.01元，预计1-2个工作日到账，请及时在“我的—我的投资”中查看，欢迎继续投资。".length());
+    }
+
+}
