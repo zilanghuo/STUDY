604642417e835c1d64ad61b622bda39302ff743e
laiwf <laiwf@zendaimoney.com>
2017-09-13 15:53:48 +0800

    查询删除

--- mouse-study/study/src/main/java/com/mouse/study/test/es/GetApiDemo.java
+++ mouse-study/study/src/main/java/com/mouse/study/test/es/GetApiDemo.java
@@ -1,8 +1,11 @@
 package com.mouse.study.test.es;
 
-import com.mouse.study.utils.JackJsonUtil;
-import org.elasticsearch.action.get.GetResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.sort.SortOrder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -16,14 +19,42 @@ public class GetApiDemo {
 
 
     public static void main(String[] args) throws Exception {
-        TransportClient client = ConfigService.getClient();
+       /* TransportClient client = ConfigService.getClient();
         GetResponse response = client.prepareGet("test03", "geo01", "AV3o6ge0DeQIl-pxDb6j").get();
         logger.info(response.getId());
         logger.info("-----" + response.getSource());
         logger.info(response.getId());
 
-        System.out.println(JackJsonUtil.objToStr(response));
+        System.out.println(JackJsonUtil.objToStr(response));*/
+
+        testFuzz();
+    }
+
+    public static void testFuzz() throws Exception {
+        TransportClient client = ConfigService.getClient();
+       /* SearchRequestBuilder requestBuilder = client.prepareSearch("dmp").setTypes("businessReportMapping")
+                .setQuery(QueryBuilders.queryStringQuery("条数据").allowLeadingWildcard(true)
+                        .autoGeneratePhraseQueries(true)
+                        systemMapping
+                        businessReportMapping
+                        businessFeatureMapping
+                );*/
 
 
+        SearchRequestBuilder requestBuilder = client.prepareSearch("dmp").setTypes("businessReportMapping")
+                .setQuery(QueryBuilders.queryStringQuery("\"批量数据273\""));
+
+        System.out.println(requestBuilder.toString());
+        SearchResponse response = requestBuilder
+                .setFrom(0)
+                .addSort("createTime", SortOrder.DESC)
+                .setSize(300)
+                .execute().actionGet();
+        SearchHit[] searchHits = response.getHits().getHits();
+        System.out.println("总大小:" + response.getHits().getTotalHits());
+        System.out.println("大小:" + searchHits.length);
+        for (SearchHit hit : searchHits) {
+            System.out.println("hit:" + hit.getSourceAsString());
+        }
     }
 }
--- mouse-study/study/src/main/java/com/mouse/study/test/es/mapping/MappingDemo.java
+++ mouse-study/study/src/main/java/com/mouse/study/test/es/mapping/MappingDemo.java
@@ -39,7 +39,7 @@ public class MappingDemo {
     private static void getMapping() throws Exception {
         TransportClient client = ConfigService.getClient();
         ImmutableOpenMap<String, MappingMetaData> mappings = client.admin().cluster().prepareState().execute()
-                .actionGet().getState().getMetaData().getIndices().get("dmp").getMappings();
+                .actionGet().getState().getMetaData().getIndices().get("dmp_dev").getMappings();
         for (ObjectObjectCursor<String, MappingMetaData> cursor : mappings) {
             System.out.println(cursor.key); // 索引下的每个type
             System.out.println(cursor.value.getSourceAsMap()); // 每个type的mapping
@@ -173,7 +173,7 @@ public class MappingDemo {
      * @throws Exception
      */
     public static void createBangMapping() throws Exception {
-        PutMappingRequest mapping = Requests.putMappingRequest("test111").type("testlog").source(GeoMapping.getMapping());
+        PutMappingRequest mapping = Requests.putMappingRequest("dmp_dev").type("test01").source(GeoMapping.getMapping());
         TransportClient client = ConfigService.getClient();
         client.admin().indices().putMapping(mapping).actionGet();
 
