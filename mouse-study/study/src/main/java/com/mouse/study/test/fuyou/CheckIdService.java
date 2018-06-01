44093200c2277bc58f8ffd3d4afc7933872f77bb
laiwf <laiwf@zendaimoney.com>
2018-06-01 09:20:38 +0800

    去除敏感字符

--- mouse-study/study/src/main/java/com/mouse/study/test/fuyou/CheckIdService.java
+++ mouse-study/study/src/main/java/com/mouse/study/test/fuyou/CheckIdService.java
@@ -1,10 +1,12 @@
 package com.mouse.study.test.fuyou;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.mouse.study.test.fuyou.req.CheckIdReqData;
 import com.mouse.study.test.fuyou.res.CheckIdRespData;
 import com.mouse.study.utils.JackJsonUtil;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 /**
@@ -14,7 +16,7 @@ import java.util.Map;
  */
 public class CheckIdService {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         CheckIdReqData data = new CheckIdReqData();
         data.setIdNo("350322199112096814");
         data.setMchntOrderid("2016021600001");
@@ -24,12 +26,17 @@ public class CheckIdService {
 
         Map<String, String> params = new HashMap();
         params.put("FM", data.buildXml());
-        System.out.println("请求报文:"+params);
+        System.out.println("请求报文:" + params);
         String respStr = HttpPostUtil.postForward(Constants.CHECK_ID_REQ_URL, params);
-        System.out.println("返回报文:"+respStr);
-        CheckIdRespData respData= XmlBeanUtils.convertXml2Bean(respStr, CheckIdRespData.class);
+        System.out.println("返回报文:" + respStr);
+        CheckIdRespData respData = XmlBeanUtils.convertXml2Bean(respStr, CheckIdRespData.class);
         System.out.println(JackJsonUtil.objToStr(respData));
 
+        Map<String, Object> map = BeanUtil.beanToMap(data);
+        Iterator<Map.Entry<String, Object>> iterator = map.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, Object> next = iterator.next();
+            System.out.println(next.getKey() + ":" + next.getValue());
+        }
     }
-
 }
